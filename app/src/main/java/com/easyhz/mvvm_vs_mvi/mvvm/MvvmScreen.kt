74effa330c0528352c972534e7274099bed0ff3c
easyhz <jiton3@naver.com>
2024-06-05 18:02:46 +0900

    feat: mvvm 예시 추가

--- app/src/main/java/com/easyhz/mvvm_vs_mvi/mvvm/MvvmScreen.kt
+++ app/src/main/java/com/easyhz/mvvm_vs_mvi/mvvm/MvvmScreen.kt
@@ -1,2 +1,41 @@
 package com.easyhz.mvvm_vs_mvi.mvvm
 
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.easyhz.mvvm_vs_mvi.domain.Post
+import com.easyhz.mvvm_vs_mvi.util.Loading
+
+@Composable
+fun MvvmScreen(
+    navController: NavHostController,
+    viewModel: MvvmViewModel = hiltViewModel()
+) {
+    MvvmScreenContent(
+        post = viewModel.post,
+        isLoading = viewModel.isLoading,
+        isLiked = viewModel.isLiked,
+        onToggleLike = { viewModel.toggleLike() } ,
+        onBackClick = { navController.navigateUp() }
+    )
+}
+
+@Composable
+private fun MvvmScreenContent(
+    post: Post?,
+    isLoading: Boolean,
+    isLiked: Boolean,
+    onToggleLike: () -> Unit,
+    onBackClick: () -> Unit
+) {
+    if (isLoading) {
+        // 로딩 이벤트
+        Loading()
+    } else {
+        /*
+         * `post` 내용
+         * `isLiked`의 표기
+         * `onToggleLike`, `onBackClick` 등의 이벤트가 발생할 수 있음
+         */
+    }
+}
--- app/src/main/java/com/easyhz/mvvm_vs_mvi/mvvm/MvvmViewModel.kt
+++ app/src/main/java/com/easyhz/mvvm_vs_mvi/mvvm/MvvmViewModel.kt
@@ -1,4 +1,42 @@
 package com.easyhz.mvvm_vs_mvi.mvvm
 
-class MvvmViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.easyhz.mvvm_vs_mvi.domain.Post
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class MvvmViewModel(
+    private val savedStateHandle: SavedStateHandle
+): ViewModel() {
+
+    var post by mutableStateOf<Post?>(null)
+        private set
+    var isLoading by mutableStateOf(false)
+        private set
+    var isLiked by mutableStateOf(false)
+        private set
+
+    init {
+        savedStateHandle.get<String>("id")?.let { id ->
+            loadPost(id)
+        }
+    }
+
+    fun toggleLike() {
+        post?.let {
+            post = it.copy(isLiked = !it.isLiked)
+        }
+    }
+
+    private fun loadPost(id: String) = viewModelScope.launch {
+//        isLoading = true
+//        post = repository.fetchPost(id)
+//        isLoading = false
+    }
 }
