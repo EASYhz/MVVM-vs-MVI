cb8a0f71712ad90b2a0c3cd5865be97508a36356
easyhz <jiton3@naver.com>
2024-06-05 18:39:43 +0900

    feat: mvi 예시 추가

--- app/src/main/java/com/easyhz/mvvm_vs_mvi/mvi/MviScreen.kt
+++ app/src/main/java/com/easyhz/mvvm_vs_mvi/mvi/MviScreen.kt
@@ -1,2 +1,35 @@
 package com.easyhz.mvvm_vs_mvi.mvi
 
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.easyhz.mvvm_vs_mvi.util.Loading
+
+@Composable
+fun MviScreen(
+    navHostController: NavHostController,
+    viewModel: MviViewModel = hiltViewModel()
+) {
+    MviScreenContent(
+        state = viewModel.state,
+        onAction = viewModel::onAction
+    )
+}
+
+@Composable
+private fun MviScreenContent(
+    state: MviState,
+    onAction: (MviAction) -> Unit
+) {
+    if (state.isLoading) {
+        // 로딩
+        Loading()
+    } else {
+        /*
+        * `post` 내용
+        * `isLiked`의 표기
+        * `onToggleLike`, `onBackClick` 등이 발생할 수 있음
+        */
+    }
+
+}
--- app/src/main/java/com/easyhz/mvvm_vs_mvi/mvi/MviViewModel.kt
+++ app/src/main/java/com/easyhz/mvvm_vs_mvi/mvi/MviViewModel.kt
@@ -1,4 +1,55 @@
 package com.easyhz.mvvm_vs_mvi.mvi
 
-class MviViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.easyhz.mvvm_vs_mvi.domain.Post
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+
+
+data class MviState(
+    val post: Post? = null,
+    val isLoading: Boolean = false,
+    val isLiked: Boolean = false
+)
+
+sealed interface MviAction {
+    data object ToggleLike: MviAction
+}
+
+@HiltViewModel
+class MviViewModel(
+    private val savedStateHandle: SavedStateHandle
+): ViewModel() {
+
+    var state by mutableStateOf(MviState())
+        private set
+
+    init {
+        savedStateHandle.get<String>("id")?.let { id ->
+            loadPost(id)
+        }
+    }
+
+    fun onAction(action: MviAction) {
+        when(action) {
+            MviAction.ToggleLike -> toggleLike()
+            else -> Unit
+        }
+    }
+
+    private fun toggleLike() {
+        state.post?.let {
+            state = state.copy(
+                post = it.copy(isLiked = !it.isLiked)
+            )
+        }
+    }
+    private fun loadPost(id: String) = viewModelScope.launch {
+        // 글 불러오는 코드 ..
+    }
 }
